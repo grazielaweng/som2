c59a30342e3bd3900bf55ae20e643bfb59471f70
grazielaweng <125660385+grazielaweng@users.noreply.github.com>
2023-10-20 10:52:03 -0300

    er
    r

---
+++ main.js
@@ -0,0 +1,8 @@
+function tocaSomPom(){
+    document.querySelector('#som tecla_pom')
+}
+    document.querySelector('tecla_pom').play();
+
+    document.querySelectorAll('.tecla');
+    const listaDeTeclas=querySelectorAll('.tecla');
+    listaDeTeclas[0].onclick=tocaSomPom;
