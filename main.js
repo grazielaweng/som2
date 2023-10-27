6e4fd8faad7080d85facb6ec23e185286638fda6
grazielaweng <125660385+grazielaweng@users.noreply.github.com>
2023-10-27 10:49:08 -0300

    h
    h

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-function tocaSomPom(){
-    document.querySelector('#som tecla_pom')
-}
-    document.querySelector('tecla_pom').play();
+//crianda a funçao tocaSomAplauso e buscando id da tecla para tocar.
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play()
 
-    document.querySelectorAll('.tecla');
-    const listaDeTeclas=querySelectorAll('.tecla');
-    listaDeTeclas[0].onclick=tocaSomPom;
+}
+//referencia constante listsDeTeclas que busca todas as classes tecla
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//criar a variavel de contagem
+let contador = 0;
+//laco de repetiçao while
+while(contador < listaDeTeclas.length){ 
+    //faz funcionar som com teclas.
+    const efeito = listaDeTeclas[contador]. classlist[1];
+    const idAudio = "#som_"+ efeito;
+    listaDeTeclas[contador].onclick =function(){
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+    console,log(contador);  
+}
